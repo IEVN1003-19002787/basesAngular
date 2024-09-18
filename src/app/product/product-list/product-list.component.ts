4e0a6d2f5b6141ba04d3075446b1d0239745fd9d
IEVN1003-19002787 <carlos.quirogaprado@gmail.com>
2024-09-17 19:39:52 -0600

    martes 17 ocultar img

---
+++ src/app/product/product-list/product-list.component.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl:'./product-list.component.html', 
+  styleUrl: './product-list.component.css'
+})
+export class ProductListComponent {
+  title="saludo de variable";
+
+  imageWidth:number=50;
+  imageMargi:number=2;
+  muestraImg:boolean=true;
+  listFilter:string='';
+
+  showImage():void{
+    this.muestraImg=!this.muestraImg;
+  }
+  productos:any[]=[
+    {
+      "productoId":1,
+      "Modelo":'Sentra',
+      "Descripcion":"4 puertas familiar",
+      "year":"febrero 3 2022",
+      "precio":2000,
+      "Marca":"NISSAN",
+      "Color":"Moradito",
+      "ImagenUrl":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSP-upxFnmgf08oghvYTLN4NZatKZ9lH5RjA&s"
+    },
+    {
+      "productoId":2,
+      "Modelo":'Beetle',
+      "Descripcion":"4 puertas familiar",
+      "year":"febrero 5 2022",
+      "precio":3000,
+      "Marca":"volkswagen",
+      "Color":"Camaleon",
+      "ImagenUrl":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMRzyTyXSrGKEoEeSGz5optp595Cim9WC54Awo84pdJqfBHagjKPyw8V8TmNYFQnDv-g8&usqp=CAU"
+    },
+    {
+      "productoId":3,
+      "Modelo":'Tsuru',
+      "Descripcion":"4 puertas deportivo",
+      "year":"febrero 3 2020",
+      "precio":7000,
+      "Marca":"Lamborghini",
+      "Color":"Verde",
+      "ImagenUrl":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4-Y_mKxChDlYrlz3uPl57BHk10kWE0EFsRdtFz8MJFz4B0dhzML2pGXKC-EYLdga9kZY&usqp=CAU"
+    }
+  ]
+}
